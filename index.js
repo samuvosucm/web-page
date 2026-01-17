e894efea080876ef83edec340ad591a833db39f4
samuvosucm <samper04@ucm.es>
2026-01-17 18:34:16 +0100

    commit inicial

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan')
+const path = require('path');
+const session = require('express-session');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: 'ecomarket-exam',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => {
+    console.log(`EcoMarket EXAM app running at http://localhost:${PORT}`);
+})
